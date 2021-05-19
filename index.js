c5d7e8f9353f4250a324d6c5e97dffaf235f9520
DanielAbedoy <dany_abedoy@live.com>
2021-05-19 10:04:01 -0500

    favicon

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const http = require('http');
 const SocketClass = require('./src/app/socket');
 
 
+
 const app = express();
+const server = http.createServer(app)
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-const server = app.listen(port, () => {
+server.listen(port, () => {
   console.log("Servidor corriendo en el puerto " + port);
 })
 
